a5b93d82c2dacc1bc9c2505ee8dd014a7a4abf7d
20PW14 <karthickprnv@gmail.com>
2023-01-24 23:13:18 +0530

    commit after jwt, before nest guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,11 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import { hash, verify } from "argon2";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { JwtService } from "@nestjs/jwt/dist";
+import { ConfigService } from "@nestjs/config/dist";
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {
+    constructor(private prisma: PrismaService, private jwt: JwtService, private config: ConfigService) {
 
     }
     async register(dto: AuthDto) {
@@ -18,8 +20,7 @@ export class AuthService {
                     hash: hashedPassword,
                 },
             });
-            delete user.hash;
-            return user;
+            return this.signToken(user.id, user.email);
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
                 if (error.code === 'P2002') {
@@ -50,7 +51,29 @@ export class AuthService {
             );
         }
 
-        delete user.hash;
-        return user;
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(
+        userId: number,
+        email: string,
+    ): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email,
+        };
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(
+            payload,
+            {
+                expiresIn: '15m',
+                secret: secret,
+            },
+        );
+
+        return {
+            access_token: token,
+        };
     }
 }
