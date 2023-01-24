9c52dfcdddb7d416175054628d7c3c8605e4e431
20PW14 <karthickprnv@gmail.com>
2023-01-24 22:42:32 +0530

    commit before auth with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,56 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
-
-@Injectable({})
+import { AuthDto } from "./dto";
+import { hash, verify } from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+@Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {
 
     }
-    login() {
-        return { message: `LOGGED IN`, error: false };
+    async register(dto: AuthDto) {
+        const hashedPassword = await hash(dto.password);
+
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash: hashedPassword,
+                },
+            });
+            delete user.hash;
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken.');
+                }
+            }
+            throw error;
+        }
     }
-    register() {
-        return { message: `REGISTRATION TIME`, error: false };
+    async login(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        });
+
+        if (!user) {
+            throw new ForbiddenException(
+                'Credentials incorrect.'
+            );
+        }
+
+        const pwMatches = await verify(user.hash, dto.password);
+
+        if (!pwMatches) {
+            throw new ForbiddenException(
+                'Passwords don\'t match.'
+            );
+        }
+
+        delete user.hash;
+        return user;
     }
 }
--- startup.md
+++ startup.md
@@ -25,5 +25,68 @@ We wrote our schema in the schema.prisma file and then we ran the following comm
     npx prisma migrate dev --name init
 ```
 
+We created AuthDto interface in the src/auth/dto dir
+
+We installed class-validator and class-transformer
+
+```
+    npm i class-validator class-transformer
+```
+
+Added validators in the AuthDto
+
+```
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+```
+
+added the following to the main.ts file
+
+```
+    app.useGlobalPipes(new ValidationPipe());
+```
+add white list to the ValidationPipe
+
+```
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+    }));
+```
+
+Installed argon2 for hashing passwords
+
+```
+    npm i argon2
+```
 
 
+Now inside auth/auth.service.ts we added the code to do the following
+
+1. Generate the password hash
+2. Save the new user in the DB
+3. Return the saved user
+
+
+We installed @nestjs/config and added the following to the main.ts file to load the .env file
+
+```
+    npm i @nestjs/config
+```
+
+Now we install passport and @nestjs/passport
+
+```
+    npm i passport @nestjs/passport
+```
+
+Then we installed passport-jwt and @nestjs/jwt with types
+
+```
+    npm i passport-jwt @nestjs/jwt
+    npm i -D @types/passport-jwt
+```
